5a6999c939f58b15fb2a7ee651c593596a96d4f8
davidvidovic <david.vidovic2000@gmail.com>
2023-11-13 11:08:10 +0100

    Started work od decoding instructions.

--- src/cpu.c
+++ src/cpu.c
@@ -22,4 +22,40 @@ void cpu_store(CPU* cpu, uint64_t addr, uint64_t size, uint64_t value)
 	bus_store(&(cpu->bus), addr, size, value);
 }
 
+uint64_t rd(uint32_t instruction) 
+{
+	return (instruction >> 7) & 0x1f;	// rd in bits 11..7
+}
 
+uint64_t rs1(uint32_t instruction) 
+{
+	return (instruction >> 15) & 0x1f;	// rs1 in bits 19..15
+}
+
+uint64_t rs2(uint32_t instruction)
+{
+	return (instruction >> 20) & 0x1f;	// rs2 in bits 24..20
+}
+
+uint64_t imm_I(uint32_t instruction)
+{
+	// I-type imm is in bits 31..20
+	return (uint64_t)((instruction & 0xfff00000) >> 20);
+}
+
+uint64_t imm_S(uint32_t instruction)
+{
+	// S-type imm is in bits 31..25 | 11.7
+	return (uint64_t)(((instruction & 0xfe000000) >> 20) | ((instruction >> 7) & 0x1f));
+}
+
+uint64_t imm_B(uint32_t instruction)
+{
+	// B-type imm is in bits 31 | 30..25 | 11..8 | 7
+	return (uint64_t)(((instruction & 0x80000000) >> 19) | ((instruction & 0x80 << 4)) | ((instruction >> 20) & 0x7e0) | ((instruction >> 7) & 0x1e));
+}
+
+uint64_t imm_U(uint32_t instruction)
+{
+	
+}
